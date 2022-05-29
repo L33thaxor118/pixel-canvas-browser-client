7caa223ae076288c159561dd2f3366965e9ad6ce
Azriel Alvarado <alvarado_118@yahoo.com>
2022-05-29 01:29:57 -0500

    Update package.json

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@ const socket = new WebSocket('ws://localhost:8080');
 interface PaintCommand {
   x: number,
   y: number,
-  color: string
+  color: number
 }
 
+/**
+ * Represent the canvas state collapsed to a single dimension, 
+ * with every 16 elements representing a row starting with the topmost one.
+ */
 type MatrixStateDTO = Array<number>
 
+/**
+ * Represents canvas state in 2 dimensions
+ */
 type MatrixState = Array<Array<number>>
-
 
 type MatrixStateListener = (event: MatrixState)=>void
 
 var stateListener: MatrixStateListener | null = null
 
 socket.addEventListener('message', function (event) {
-  console.log("received state from server: " + event.data)
   const stateDto: MatrixStateDTO = JSON.parse(event.data)
   var matrixState: MatrixState = []
   const size = 16
@@ -28,7 +33,13 @@ socket.addEventListener('message', function (event) {
   }
 });
 
-function paintTile(x: number, y: number, color: string) {
+/**
+ * Paints a pixel on the canvas
+ * @param x - x position of pixel to paint, assuming x=0,y=0 is top-left corner
+ * @param y - y position of pixel to paint, assuming x=0,y=0 is top-left corner
+ * @param color - decimal representation of the color to paint
+ */
+function paintTile(x: number, y: number, color: number) {
   const message: PaintCommand = {
     x: x,
     y: y,
